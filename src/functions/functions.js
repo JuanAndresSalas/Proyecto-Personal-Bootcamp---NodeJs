d7278741a891d7d3e934757da6bdc101704ba260
Juan Salas <juan.salas.salas@outlook.com>
2023-03-24 04:37:47 -0300

    Primera version login

---
+++ src/functions/functions.js
@@ -0,0 +1,10 @@
+ import * as fs from "fs"
+ 
+ export async function leerArchivo(url) {
+    let data = await fs.promises.readFile(url, (err, data) => {
+        if (err) throw err 
+        return data
+    });
+    return await JSON.parse(data);
+}
+
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,12 +1,56 @@
+//Importación
 import { Router } from "express";
 import bodyParser from "body-parser";
 import { body, validationResult } from 'express-validator';
-const router = Router()
+import passport from "passport";
+import PassportLocal from "passport-local"
+import session from "express-session"
+import cookieParser from "cookie-parser";
 import fs from "fs"
+
+import {leerArchivo} from "../functions/functions.js"
+
+//Constantes
+const router = Router()
+const PassPortLocal = PassportLocal.Strategy
+const rutaUsuarios = "data/usuarios.json"
+
+
+//Middleware
 
 router.use(bodyParser.json({limit: "50mb"}));
 router.use(bodyParser.urlencoded({limit: "50mb", extended: true, parameterLimit:50000}))
+router.use(cookieParser("secreto"))
+router.use(session({
+    secret:"secreto",
+    saveUninitialized: true, 
+    resave:true
+}))
+router.use(passport.initialize())
+router.use(passport.session())
 
+passport.use("local",new PassPortLocal(function(username,password,done){
+    if(username == "juan.salass@outlook.com" && password == "1234"){
+               
+        return done(null,{id:1, name:"juan"})
+    
+    }
+    done(null,false)
+    
+}))
+
+passport.serializeUser(function(user, done) {
+    done(null, user.id);
+});
+
+passport.deserializeUser(function(id, done) {
+
+    done(null,{id:1, name:"juan"})
+
+});
+
+
+ 
 //Rutas
 router.get("/", (req, res) =>{
     res.render("index")
@@ -18,6 +62,10 @@ router.get("/index", (req,res) => {
 
 router.get("/subir-oferta",(req, res) =>{
     res.render("subir-oferta")
+})
+
+router.get("/login", (req, res) =>{
+    res.render("login")
 })
 
 router.post("/oferta-nueva",
@@ -40,8 +88,13 @@ router.post("/oferta-nueva",
         let lugar = req.body.lugar
         let precio = req.body.precio
         let descripcion = req.body.descripcion
+       
         
     }
 })
 
+router.post("/ingreso",passport.authenticate("local",{
+                            successRedirect: "/",
+                            failureRedirect: "login"
+                        }))
 export default router //Exportar el contenido de este archivo
